b1491808cb1eda1d409670168db337b6440053b5
Koen Betsens <betsens@Koens-MacBook-Air.local>
2017-12-05 09:48:43 +0100

    minor

--- swagger_server/models/alarm.py
+++ swagger_server/models/alarm.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import
 # from .base_model_ import Model
 from datetime import date, datetime
-from typing import List, Dict
+#from typing import List, Dict
 from ..util import deserialize_model
 
 from sqlalchemy import Table, Column, ForeignKey, String, Integer, Text
